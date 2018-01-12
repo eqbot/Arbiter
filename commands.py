bc76606953684401f776c9fd8055a2f974fbac31
Eqbot <trentkenn8@gmail.com>
2018-01-12 10:58:44 -0800

    Added help command, begun work on config files and permissions system.

--- commands.py
+++ commands.py
@@ -1,10 +1,11 @@
 import helpers
 
 class Command:
-    def __init__(self, name="", alias=[],cmd=""):
+    def __init__(self, name="", alias=[],cmd="",helptext=""):
         self.name = name #Name of command, automatically checks for match
         self.alias = alias #List of regexes to run program if there's a match
         self.cmd = cmd #Command to execute
+        self.helptext = helptext
         
     def execute(self, args=None):
         output = ""
@@ -13,3 +14,8 @@ class Command:
     
 allCommands = []
 #Built in commands go here
+bHelp = Command("help",helptext="Displays the help list")
+bHelp.cmd = """for command in allCommands:
+    output += command.helptext
+    output += '\n'"""
+allCommands.append(bHelp)
--- main.py
+++ main.py
@@ -6,10 +6,40 @@ v1.0
 
 import discord
 import asyncio
+import commands
+import shlex
+import configparser
+import sys
 
 client = discord.Client()
 
-
-
+#setup config
+config = configparser.ConfigParser()
+if config.read('config.ini') == []:
+    config['VALUES'] = {'token': ''}
+    print('Config file needs to be set up before use.')
+    config.write('config.ini')
+    sys.exit()
+else:
+    token = config['VALUES']['token']
+    
+users = configparser.ConfigParser()
+if users.read('users.ini') == []:
+    config['ROOT'] = {'username': 'root',
+                      'discriminator': 1337,
+                      'powerlevel': 0
+                      }
 @client.event
 async def on_message(message):
+    if message.content.startswith('!'):
+        msgIn = message.content[1:]
+        msgIn = shlex.split(msgIn)
+        async for command in commands.allCommands:
+            if msgIn[0] == command.name:
+                (out,outFile) = command.execute(msgIn[1:])
+                if out is not None:
+                    await client.send_message(message.channel, out)
+                if outFile is not None:
+                    await client.send_file(message.channel, outFile)
+                    
+client.run()
